b7f2981b81a35545aac6a7b0ba1bbc9b378349ce
Tam <tam.nguyen@xolv.org>
2026-08-18 18:19:32 +0700

    phase 6: add negative tests and data-driven tests

---
+++ tests/login-negative.spec.ts
@@ -0,0 +1,29 @@
+import {test, expect} from '@playwright/test';
+
+//Negative test 1: login with wrong credential
+test('login with wrong credentials show error', async({page}) => {
+    await page.goto('https://demoqa.com/login');
+    await page.getByPlaceholder('Username').fill('wronguser');
+    await page.getByPlaceholder('Password').fill('wrongpass');
+    await page.getByRole('button', {name: 'Login'}).click();
+    await expect(page.getByText('Invalid username or password!')).toBeVisible();
+});
+
+//Negative test 2: data-driven with test.each
+const invalidCredentials = [
+    {username: 'wronguser', password: 'wrongpassword', label: 'wrong username and password'},
+    {username: 'testUser', password: 'wrongpassword', label: 'wrong password only'},
+    {username: 'wronguser', password:'Test@1234', label: 'wrong username only'}
+]
+
+
+for (const { username, password, label } of invalidCredentials) {
+    test(`login fails: ${label}`, async({page}) => {
+        await page.goto('https://demoqa.com/login');
+        await page.getByPlaceholder('Username').fill(username);
+        await page.getByPlaceholder('Password').fill(password);
+        await page.getByRole('button', {name: 'Login'}).click();
+        await expect(page.getByText('Invalid username or password!')).toBeVisible();
+
+    });
+}
